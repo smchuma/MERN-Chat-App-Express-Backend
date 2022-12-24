a8fe32b5adf27fcfc235d845e5e7bb3d4d2f0acc
Samora Laurent <laurentsamora6@gmail.com>
2022-12-25 00:54:03 +0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const chats = require("./data");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("api is running ");
+});
+
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  const singleChat = chats.find((one) => one._id === req.params.id);
+  res.send(singleChat);
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log("server listening on " + port));
