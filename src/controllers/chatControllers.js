2edbefbd263ffd89cfcc9792a21229e3322035ff
Samora Laurent <laurentsamora6@gmail.com>
2023-01-21 06:54:36 +0300

    created group api

--- src/controllers/chatControllers.js
+++ src/controllers/chatControllers.js
@@ -68,4 +68,33 @@ const fetchChats = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { accessChat, fetchChats };
+const createGroupChat = asyncHandler(async () => {
+  if (!req.body.users || !req.body.name) {
+    return res.status(400).send({ message: "Please fill all the fields" });
+  }
+  var users = JSON.parse(req.body.users);
+
+  if (users.length < 2) {
+    return res.status(400).send({ message: "Please select atleast 2 users" });
+  }
+  users.push(req.user);
+
+  try {
+    const groupChat = await Chat.create({
+      chatName: req.body.name,
+      users: users,
+      isGroupChat: true,
+      groupAdmin: req.user,
+    });
+    const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    res.status(200).json(fullGroupChat);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+module.exports = { accessChat, fetchChats, createGroupChat };
